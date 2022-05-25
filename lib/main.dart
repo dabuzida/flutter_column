94533ea2c3276ed47cc83d6b05ab7d46727ee676
dabuzida <dabuzida@gmail.com>
2022-05-25 14:21:15 +0900

    220525

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Column',
-      home: AAA(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Column'),
+          elevation: 0,
+          foregroundColor: Colors.cyanAccent,
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.greenAccent[100],
+        body: const AAA(),
+      ),
     );
   }
 }
@@ -21,44 +31,35 @@ class AAA extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Column'),
-        elevation: 0,
-        foregroundColor: Colors.cyanAccent,
-        backgroundColor: Colors.black,
-        centerTitle: true,
+    return Container(
+      width: 1000,
+      height: 800,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.purpleAccent, width: 2),
       ),
-      body: Container(
-        width: 1000,
-        height: 800,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.purpleAccent, width: 2),
-        ),
-        // width: double.infinity,
-        // height: double.infinity,
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.amber, width: 2),
-              ),
-              child: Text('data'),
+      // width: double.infinity,
+      // height: double.infinity,
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.amber, width: 2),
             ),
-            Text('data'),
-            Text('data'),
-            Text('data'),
-            Text('data'),
-            // Row(
-            //   children: [
-            //     Container(width: 50, height: 50, color: Colors.teal),
-            //   ],
-            // )
-          ],
-        ),
+            child: Text('data'),
+          ),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+          // Row(
+          //   children: [
+          //     Container(width: 50, height: 50, color: Colors.teal),
+          //   ],
+          // )
+        ],
       ),
     );
   }
